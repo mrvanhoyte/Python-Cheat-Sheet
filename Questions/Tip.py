9c90ff79fc14392485ccfd7cb96500e5778df9fb
mrvanhoyte <johndoe@example.commrvanhoyte@aol.com>
2022-03-09 19:46:18 -0400

    linked

---
+++ Questions/Tip.py
@@ -0,0 +1,11 @@
+bill = 47.28
+tip = bill *(15/100)
+total = bill +tip
+share = total /2
+
+print("Each person need to pay "+ str(share))
+
+print("bill = " +str(bill))
+print("tip= "+ str(tip))
+print("Total = "+ str(total))
+print("Therefore")
---
+++ Questions/word_add_space.py
@@ -0,0 +1,9 @@
+word1 = "How"
+word2 = "do"
+word3 = "you"
+word4 = "like"
+word5 = "python"
+word6 = "so"
+word7 = "far?"
+
+print(str(word1)+" "+str(word2)+" "+str(word3)+" "+str(word4)+" "+str(word5)+" "+str(word6)+" "+str(word7))

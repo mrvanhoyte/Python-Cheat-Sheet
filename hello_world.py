171412eb9a01a81f960cff18922c876d8f8ab468
Seaweed31 <101291145+Seaweed31@users.noreply.github.com>
2022-03-09 16:31:58 -0400

    completed

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,6 @@
+<<<<<<< HEAD
+# Your task in this one is to PRINT HELLO WORLD
+print("Hello World")
+=======
 # Your task in this one is to PRINT HELLO WORLD.
+>>>>>>> 8773bd866447e61ffce47d7b449657006970c1f4
